e8e0820f7321f4da2e0b5e65a6d804430bc2a7ad
Francisco Júnior <franciscojr.dev@gmail.com>
2023-12-13 20:48:05 -0300

    Changes

--- src/Request.php
+++ src/Request.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace FjrSoftware\Flinkbot\Request;
 
+use Closure;
 use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
 use Psr\Http\Message\ResponseInterface;
@@ -13,6 +14,8 @@ class Request
     private Client $client;
 
     private array $options = [];
+
+    private Closure $callback;
 
     public function __construct(
         private readonly string $url,
@@ -48,23 +51,45 @@ class Request
         return $this->options + $options;
     }
 
+    public function setCallbackRequest(callable $callback): void
+    {
+        $this->callback = $callback;
+    }
+
+    private function callbackRequest(ResponseInterface $response): ResponseInterface
+    {
+        if ($this->callback) {
+            call_user_func($this->callback, $response);
+        }
+
+        return $response;
+    }
+
     public function get(string $uri = '', array $options = []): ResponseInterface
     {
-        return $this->client->request('GET', $uri, $this->getOptions($options));
+        return $this->callbackRequest(
+            $this->client->request('GET', $uri, $this->getOptions($options))
+        );
     }
 
     public function post(string $uri = '', array $options = []): ResponseInterface
     {
-        return $this->client->request('POST', $uri, $this->getOptions($options));
+        return $this->callbackRequest(
+            $this->client->request('POST', $uri, $this->getOptions($options))
+        );
     }
 
     public function delete(string $uri = '', array $options = []): ResponseInterface
     {
-        return $this->client->request('DELETE', $uri, $this->getOptions($options));
+        return $this->callbackRequest(
+            $this->client->request('DELETE', $uri, $this->getOptions($options))
+        );
     }
 
     public function put(string $uri = '', array $options = []): ResponseInterface
     {
-        return $this->client->request('PUT', $uri, $this->getOptions($options));
+        return $this->callbackRequest(
+            $this->client->request('PUT', $uri, $this->getOptions($options))
+        );
     }
 }
